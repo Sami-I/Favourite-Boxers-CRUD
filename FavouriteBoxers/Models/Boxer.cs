cd70f4c2e25a2b645120f2144afc885ff0073115
Sami <sami_khalid@hotmail.co.uk>
2022-06-08 01:09:19 +0100

    Fixed DOB

--- FavouriteBoxers/Models/Boxer.cs
+++ FavouriteBoxers/Models/Boxer.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace FavouriteBoxers.Models
 {
     public class Boxer
     {
         public int Id { get; set; }
+        [Display(Name = "Full Name")]
         public string Full_Name { get; set; }
         public string Alias { get; set; }
         public string Nationality { get; set; }
         public string Stance { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "D.O.B")]
         public DateTime DOB { get; set; }
 
         public Boxer()
